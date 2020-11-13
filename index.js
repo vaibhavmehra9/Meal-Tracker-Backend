60eaabe20f7141bcef15a27e608b9fadc3ead96c
vaibhav mehra <vaibhavmehra@vaibhavs-MacBook-Air.local>
2020-11-14 00:19:19 +0530

    routes folder added

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const appRoutes = require("./routes");
 const app = express();
 
-app.listen(8080, () => {
+// middlewares
+
+app.use(express.json());
+
+app.use("/", appRoutes);
+
+app.listen(8080, (err) => {
+  if (err) {
+    console.log(`Error while starting server: ${err}`);
+  }
   console.log(`Server is up and running`);
 });
