75d43b02496d01524caffa8f72f0eb92832cf303
vaibhav mehra <vaibhavmehra@vaibhavs-MacBook-Air.local>
2020-11-14 02:41:56 +0530

    login controller added

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,10 +15,12 @@ module.exports.signUpUser = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({
+    const newUser = new User({
       ...req.body,
       password: hashedPassword,
     });
+
+    await newUser.save();
 
     return res.status(201).json({ status: "success", data: { user: newUser } });
   } catch (error) {
@@ -28,4 +30,37 @@ module.exports.signUpUser = async (req, res) => {
   }
 };
 
-module.exports.logInUser = (req, res) => {};
+module.exports.logInUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const isUserExist = await User.findOne({ email: email }).exec();
+
+    if (!isUserExist) {
+      return res.status(400).json({
+        status: "fail",
+        message: "No user found with this email, please sign up",
+      });
+    }
+
+    const isPasswordMatched = await bcrypt.compare(
+      password,
+      isUserExist.password
+    );
+
+    if (!isPasswordMatched) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Email or Password is wrong !",
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ status: "success", data: { user: isUserExist } });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "fail", message: "Internal server error" });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/authController");
 const router = require("express").Router();
 
 router.post("/signup", authController.signUpUser);
+router.get("/login", userController.logInUser);
 
 module.exports = router;
